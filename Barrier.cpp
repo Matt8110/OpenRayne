5894559b6a59f7551dbe178fbf53f915a884a64a
matt8110 <matt81102@gmail.com>
2023-01-08 15:53:30 -0800

    Matched (well, tried) some more functions.

--- Barrier.cpp
+++ Barrier.cpp
@@ -2,6 +2,15 @@
 
 int CBarrier::fileVersion = 0; //Hmm...
 
+CBarrier::CBarrier()
+{
+    size.x = 5.0f;
+    size.y = 5.0f;
+    size.z = 5.0f;
+    effectClassNameList = nullptr;
+    addToPathGrid = 1;
+}
+
 //What's up with that string? Pointer to a pointer?
 void CBarrier::archive()
 {
@@ -24,6 +33,25 @@ void CBarrier::setHasChanged()
 {
     if (addToPathGrid != 0)
     {
-        //gSet->addBarier();
+        //gSet->addBarier(this);
     }
+}
+
+void CBarrier::setup()
+{
+    CBoundingBox3D bb;
+
+    CDemonActor::setup();
+    
+    //field_F4 = gSit[0x402]; 0x1008
+    field_F0 = 0;
+
+    bb.x1 = -size.x * 0.5f;
+    bb.y1 = -0.1f;
+    bb.z1 = -size.z * 0.5f;
+    bb.x2 = size.x * 0.5f;
+    bb.y2 = size.y + 0.1f;
+    bb.z2 = size.z *0.5f;
+
+    setBoundingBox(bb);
 }
--- Barrier.h
+++ Barrier.h
@@ -10,10 +10,18 @@ public:
 	char* effectClassNameList;
 	static int fileVersion;
 
+    //Probably matching, but hard to tell
+    CBarrier();
+
     //Matching
     virtual void archive();//override
     virtual int renderTransparent() override;
 
     //Should be equivelent, not sure why it won't match...
     void setHasChanged();
+
+    //Not sure why it won't match, 4 instructions happen out of order
+    //for seemingly no reason, should still be equivelent and may
+    //actually be matching.
+    void setup() override;
 };
